7d6e0d6d10d4594759722279c53a2c38a97107e7
Accdo <khyeong1002@gmail.com>
2025-03-10 10:42:26 +0900

    basic1

---
+++ BasicStudy1/Assets/Scripts/VisialveScrript.cs
@@ -0,0 +1,11 @@
+using System.Diagnostics;
+using UnityEngine;
+
+public class VisialveScrript : MonoBehaviour
+{
+    public int playerScore = 0;
+    void Start()
+    {
+
+    }
+}
---
+++ BasicStudy1/Assets/VisialveScrript.cs
@@ -0,0 +1,10 @@
+using System.Diagnostics;
+using UnityEngine;
+
+public class VisialveScrript : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+}
